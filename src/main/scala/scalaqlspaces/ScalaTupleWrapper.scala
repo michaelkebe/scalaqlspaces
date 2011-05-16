a5ac7c1c4d08876f4a4bcf937f79d7450a450568
Michael Kebe <michael.kebe@gmail.com>
2011-05-16 15:28:47 +0200

    A few more testcases added sqlspaces.log* to .gitignore

--- src/main/scala/scalaqlspaces/ScalaTupleWrapper.scala
+++ src/main/scala/scalaqlspaces/ScalaTupleWrapper.scala
@@ -4,19 +4,16 @@ import info.collide.sqlspaces.client.TupleSpace
 import info.collide.sqlspaces.commons.Tuple
 
 class ScalaTupleSpaceWrapper(ts: TupleSpace) {
-/*
   private def scalaTuple2tsTuple[T1](t:Tuple1[T1]) = {
     val tst = new Tuple
     tst add t._1
     tst
   }
-*/
   private def scalaTuple2tsTuple[T1, T2](t:Tuple2[T1, T2]) = {
     val tst = new Tuple
     tst add t._1; tst add t._2
     tst
   }
-/*
   private def scalaTuple2tsTuple[T1, T2, T3](t:Tuple3[T1, T2, T3]) = {
     val tst = new Tuple
     tst add t._1; tst add t._2; tst add t._3
@@ -27,22 +24,19 @@ class ScalaTupleSpaceWrapper(ts: TupleSpace) {
     tst add t._1; tst add t._2; tst add t._3; tst add t._4
     tst
   }
-*/
-/*
   def <<[T](t: Tuple1[T]) = {
     ts write scalaTuple2tsTuple(t)
   }
-*/
   def <<[T1, T2](t: Tuple2[T1, T2]) = {
     ts write scalaTuple2tsTuple(t)
   }
-/*
   def <<[T1, T2, T3](t: Tuple3[T1, T2, T3]) = {
     ts write scalaTuple2tsTuple(t)
   }
   def <<[T1, T2, T3, T4](t: Tuple4[T1, T2, T3, T4]) = {
     ts write scalaTuple2tsTuple(t)
   }
+  /*
   def typedTake[T1](t: Tuple, dummy: Int = 0): Option[Tuple1[T1]] = {
     val rt = ts take t
     if (rt == null) None
@@ -59,5 +53,5 @@ class ScalaTupleSpaceWrapper(ts: TupleSpace) {
     else Some((getFieldAs[T1](rt, 0), getFieldAs[T2](rt, 1), getFieldAs[T3](rt, 2)))
   }
   private def getFieldAs[T](t: Tuple, i: Int) = t.getField(i).getValue.asInstanceOf[T]
-*/
+  */
 }
--- src/test/scala/scalaqlspaces/ScalaqlSpacesTest.scala
+++ src/test/scala/scalaqlspaces/ScalaqlSpacesTest.scala
@@ -19,5 +19,33 @@ class ScalaqlSpacesTest extends FlatSpec with ShouldMatchers {
     resultTuple.getField(1).getValue.asInstanceOf[Int] should equal (1)
   }
 
+  it should "allow writing Tuple1 tuples into the TupleSpace" in {
+    val ts = new TupleSpace
+    ts << Tuple1("B")
+
+    val resultTuple = ts.read(new Tuple(classOf[String]))
+    resultTuple.getField(0).getValue.asInstanceOf[String] should equal ("B")
+  }
+
+  it should "allow writing Tuple3 tuples into the TupleSpace" in {
+    val ts = new TupleSpace
+    ts << ("String", 3.14, true)
+
+    val resultTuple = ts.read(new Tuple(classOf[String], classOf[Double], classOf[Boolean]))
+    resultTuple.getField(0).getValue.asInstanceOf[String] should equal ("String")
+    resultTuple.getField(1).getValue.asInstanceOf[Double] should equal (3.14)
+    resultTuple.getField(2).getValue.asInstanceOf[Boolean] should equal (true)
+  }
+
+  it should "allow writing Tuple4 tuples into the TupleSpace" in {
+    val ts = new TupleSpace
+    ts << ("String", 3.14, true, 9.81F)
+
+    val resultTuple = ts.read(new Tuple(classOf[String], classOf[Double], classOf[Boolean], classOf[Float]))
+    resultTuple.getField(0).getValue.asInstanceOf[String] should equal ("String")
+    resultTuple.getField(1).getValue.asInstanceOf[Double] should equal (3.14)
+    resultTuple.getField(2).getValue.asInstanceOf[Boolean] should equal (true)
+    resultTuple.getField(3).getValue.asInstanceOf[Float] should equal (9.81F)
+  }
 }
 
